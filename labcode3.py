86ab7ad4ac0a112dc6d8040e5aa43c0da456fcba
PeevaO <peeva.olesya@mail.ru>
2023-11-12 20:32:10 +0300

    labcode3.py

--- labcode3.py
+++ labcode3.py
@@ -1,25 +1,31 @@
 from tkinter import *
+from PIL import ImageTk, Image
 import string
 import random
 def clicked():
     btn.destroy()
     lbl.destroy()
-    y = str(tx.get())
+    yy = str(tx.get())
+    tx.destroy()
     randLetter1 = random.choice(string.ascii_uppercase)
     randLetter2 = random.choice(string.ascii_uppercase)
     randLetter3 = random.choice(string.ascii_uppercase)
     randLetter4 = random.choice(string.ascii_uppercase)
-    sum = str(int(y[3] + y[4] + y[5]) + int(y[0] + y[1] + y[2]))
+    sum = str(int(yy[3] + yy[4] + yy[5]) + int(yy[0] + yy[1] + yy[2]))
     if len(sum)<4:
         sum = '0' + sum
-    y = y[3] + y[4] + y[5] + randLetter1 + randLetter2 + '-' + y[0] + y[1] + y[2] + randLetter3 + randLetter4 + ' ' + sum
-    lb = Label(window, text=y, font=("Times new Roman", 30))
-    lb.grid(column=1, row=0)
-    tx.destroy()
+    yy = yy[3] + yy[4] + yy[5] + randLetter1 + randLetter2 + '-' + yy[0] + yy[1] + yy[2] + randLetter3 + randLetter4 + ' ' + sum
+
+    bg = PhotoImage(file="terraria1.png")
+    pic = Label(window, image=bg)
+    pic.place(x=0, y=0, relwidth=1, relheight=1)
+    lb = Label(window, text=yy, font=("Times new Roman", 30))
+    lb.pack(pady=50)
+    window.mainloop()
 
 window = Tk()
 window.title("Добро пожаловать в самое прекрасное приложение!")
-window.geometry('1100x550')
+window.geometry('1280x720')
 lbl = Label(window, text="Введите DEC-число из 6 знаков.", font=("Times new Roman", 30))
 lbl.grid(column=0, row=0)
 tx = Entry(window, width=10)
