86e51000d9cfe4a26bbf5cfc1bc63c6896b9d001
PeevaO <peeva.olesya@mail.ru>
2023-11-14 19:18:49 +0300

    labcode3.py

--- labcode3.py
+++ labcode3.py
@@ -3,33 +3,34 @@ from PIL import ImageTk, Image
 import string
 import random
 def clicked():
-    btn.destroy()
-    lbl.destroy()
-    yy = str(tx.get())
-    tx.destroy()
-    randLetter1 = random.choice(string.ascii_uppercase)
-    randLetter2 = random.choice(string.ascii_uppercase)
-    randLetter3 = random.choice(string.ascii_uppercase)
-    randLetter4 = random.choice(string.ascii_uppercase)
-    sum = str(int(yy[3] + yy[4] + yy[5]) + int(yy[0] + yy[1] + yy[2]))
-    if len(sum)<4:
+    window.geometry('1280x720')
+    button.destroy()
+    label_1page.destroy()
+    input_num = str(entry.get())
+    entry.destroy()
+    random_Letter1 = random.choice(string.ascii_uppercase)
+    random_Letter2 = random.choice(string.ascii_uppercase)
+    random_Letter3 = random.choice(string.ascii_uppercase)
+    random_Letter4 = random.choice(string.ascii_uppercase)
+    sum = str(int(input_num[3:]) + int(input_num[:3]))
+    if len(sum) < 4:
         sum = '0' + sum
-    yy = yy[3] + yy[4] + yy[5] + randLetter1 + randLetter2 + '-' + yy[0] + yy[1] + yy[2] + randLetter3 + randLetter4 + ' ' + sum
+    key = input_num[3:] + random_Letter1 + random_Letter2 + '-' + input_num[:3] + random_Letter3 + random_Letter4 + ' ' + sum
 
-    bg = PhotoImage(file="terraria1.png")
-    pic = Label(window, image=bg)
-    pic.place(x=0, y=0, relwidth=1, relheight=1)
-    lb = Label(window, text=yy, font=("Times new Roman", 30))
-    lb.pack(pady=50)
+    picture = PhotoImage(file="terraria1.png")
+    background = Label(window, image=picture)
+    background.place(x=0, y=0, relwidth=1, relheight=1)
+    label_2page = Label(window, text=key, font=("Times new Roman", 30))
+    label_2page.pack(pady=50)
     window.mainloop()
 
 window = Tk()
 window.title("Добро пожаловать в самое прекрасное приложение!")
-window.geometry('1280x720')
-lbl = Label(window, text="Введите DEC-число из 6 знаков.", font=("Times new Roman", 30))
-lbl.grid(column=0, row=0)
-tx = Entry(window, width=10)
-tx.grid(column=2, row=0)
-btn = Button(window, text="Получить ключ!", bg="black", fg="white", command=clicked)
-btn.grid(column=3, row=0)
+window.geometry('460x320')
+label_1page = Label(window, text="Введите DEC-число из 6 знаков.", font=("Times new Roman", 25))
+label_1page.grid(column=0, row=0)
+entry = Entry(window, width=15)
+entry.grid(column=0, row=2)
+button = Button(window, text="Получить ключ!", bg="black", fg="white", command=clicked)
+button.grid(column=0, row=3)
 window.mainloop()
